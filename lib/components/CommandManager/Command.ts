41dcaf648fee312e1f44ea927867bde568c700c4
HcgRandon <me@randon.moe>
2020-10-14 13:18:02 -0400

    Argument Helpers, Eris Types reeee

--- lib/components/CommandManager/Command.ts
+++ lib/components/CommandManager/Command.ts
@@ -3,7 +3,7 @@ import { CommandContext } from "./CommandContext";
 
 export interface Command extends Component {
 	aliases: Array<string>;
-	execute(ctx?: CommandContext): Promise<void>;
+	execute(ctx?: CommandContext): Promise<any>;
 }
 
 export interface CommandAPI extends ComponentAPI {}
--- lib/components/CommandManager/CommandContext.ts
+++ lib/components/CommandManager/CommandContext.ts
@@ -1,19 +1,26 @@
-import { Message, TextableChannel, User } from "eris";
+import { Guild, Message, TextableChannel, TextChannel, User } from "eris";
 
-export class CommandContext {
+export class CommandContext<T extends TextableChannel = TextableChannel> {
 	public readonly message: Message;
-	public readonly channel: TextableChannel;
+	public readonly channel: T;
 	public readonly author: User;
+
+	public readonly guild?: Guild;
 
 	public readonly prefix: string;
 	public readonly alias: string;
 	public readonly args: Array<string>;
 
-	public constructor(message: Message, prefix: string, alias: string, args: Array<string>) {
+	private argIndex = 0;
+
+	public constructor(message: Message<T>, prefix: string, alias: string, args: Array<string>) {
 		this.message = message;
 
 		this.channel = message.channel;
 		this.author = message.author;
+
+		// God help us
+		if ((message.channel as TextChannel).guild) this.guild = (message.channel as TextChannel).guild;
 
 		this.prefix = prefix;
 		this.alias = alias;
@@ -22,5 +29,21 @@ export class CommandContext {
 
 	get isMention() {
 		return /^<@!?[0-9]+>$/.test(this.prefix);
+	}
+
+	public nextArg() {
+		const arg = this.args[this.argIndex];
+		if (arg == null) return null;
+
+		// increment index
+		this.argIndex++;
+
+		return arg;
+	}
+
+	public remainingArgs() {
+		if (this.argIndex > this.args.length) return [];
+
+		return this.args.slice(this.argIndex);
 	}
 }
