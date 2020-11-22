9a0d19db9392a37a19ad7e8fe4a11889ab485132
HcgRandon <me@randon.moe>
2020-11-21 22:53:48 -0500

    Export PromptManager

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,11 @@
 export { Bentocord, Bentocord as default } from './Bentocord';
 export * from './BentocordVariable';
 
-export * from './discord';
 export * from './arguments';
-export * from './inhibitors';
 export * from './commands';
+export * from './discord';
+export * from './inhibitors';
+export * from './prompt';
 
 export * from './builders';
 export * from './plugins';
