7cfc3f751c117df0eaf32379dca2b697635d44ee
HcgRandon <me@randon.moe>
2021-07-28 01:03:52 +0000

    Revert "Simplified Bentocord component loading"
    This reverts commit 21ec00c1fb8639d2104a34bb596a474795476ab9

--- lib/Bentocord.ts
+++ lib/Bentocord.ts
@@ -1,16 +1,10 @@
 import 'reflect-metadata';
 
-import { FSEntityLoader, Plugin, PluginAPI } from '@ayanaware/bento';
+import { EntityType, FSEntityLoader, Plugin, PluginAPI } from '@ayanaware/bento';
 import { ClientOptions } from 'eris';
 
 import { BentocordInterface } from './BentocordInterface';
 import { BentocordVariable } from './BentocordVariable';
-
-import ArgumentManager from './arguments';
-import CommandManager from './commands';
-import Discord from './discord';
-import InhibitorManager from './inhibitors';
-import PromptManager from './prompt';
 
 import { Logger } from '@ayanaware/logger-api';
 const log = Logger.get();
@@ -40,34 +34,30 @@ export class Bentocord implements Plugin {
 	}
 
 	public async onLoad() {
-		const bento = this.api.getBento();
+		// get prexisting FSEntityLoader, or create one just for us
+		this.fsel = await this.api.getBento().getPlugin(FSEntityLoader);
+		if (!this.fsel) {
+			this.fsel = new (class extends FSEntityLoader {
+				name = '@ayanaware/bentocord:FSEntityLoader'
+			})();
+
+			await this.api.getBento().addPlugin(this.fsel);
+		}
 
 		// Load BentocordInterface
-		await bento.entities.addPlugin(BentocordInterface);
+		await this.api.getBento().addPlugin(BentocordInterface);
 
-		// Load Bentocord Components
-		for (const component of [
-			ArgumentManager,
-			CommandManager,
-			Discord,
-			InhibitorManager,
-			PromptManager
-		]) await bento.entities.addComponent(component);
+		// Load Components
+		await this.fsel.addDirectories([
+			[__dirname, 'arguments'],
+			[__dirname, 'commands'],
+			[__dirname, 'discord'],
+			[__dirname, 'inhibitors'],
+			[__dirname, 'prompt'],
+		], EntityType.COMPONENT, false);
 
 		// Load built-in commands
 		const loadBuiltin = this.api.getVariable<boolean>({ name: BentocordVariable.BENTOCORD_BUILTIN_COMMANDS, default: true });
-		if (loadBuiltin) {
-			// get prexisting FSEntityLoader, or create one just for us
-			this.fsel = await this.api.getBento().getPlugin(FSEntityLoader);
-			if (!this.fsel) {
-				this.fsel = new (class extends FSEntityLoader {
-					name = '@ayanaware/bentocord:FSEntityLoader'
-				})();
-
-				await bento.entities.addPlugin(this.fsel);
-			}
-
-			return this.fsel.addDirectory([__dirname, 'commands', 'builtin']);
-		}
+		if (loadBuiltin) return this.fsel.addDirectory([__dirname, 'commands', 'builtin']);
 	}
 }
