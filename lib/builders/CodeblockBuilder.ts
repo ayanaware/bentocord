d75a3f9a013158f6f4f66e5b272a8f332b9b8283
HcgRandon <me@randon.moe>
2021-09-08 21:31:08 -0400

    Uncomplicate CodeblockBuilder

--- lib/builders/CodeblockBuilder.ts
+++ lib/builders/CodeblockBuilder.ts
@@ -1,16 +1,13 @@
 export interface CodeblockLine { key?: CodeblockLineItem; value: CodeblockLineItem }
-export type CodeblockLineItem = number | string | boolean | CodeblockLineObject;
-export interface CodeblockLineObject {[key: string]: number | string | boolean}
-
-export type CodeblockLanguage = 'apache' | 'prolog' | 'css' | string;
+export type CodeblockLineItem = string | number | boolean;
 
 export class CodeblockBuilder {
-	public language: CodeblockLanguage = null;
+	public language: string;
+
 	private readonly lines: Array<CodeblockLine> = [];
 
-	// eslint-disable-next-line @typescript-eslint/require-await
-	public async resolveLine(item: CodeblockLineItem): Promise<string> {
-		return JSON.stringify(item);
+	public constructor(language?: string) {
+		this.language = language;
 	}
 
 	public addLine(item: CodeblockLineItem, value?: CodeblockLineItem): CodeblockBuilder {
@@ -20,7 +17,7 @@ export class CodeblockBuilder {
 		return this;
 	}
 
-	public async render(): Promise<string> {
+	public render(): string {
 		// add top
 		let render = '```';
 
@@ -31,19 +28,12 @@ export class CodeblockBuilder {
 		render += '\n';
 
 		// resolve lines
-		const lines: Array<{ key?: string, value: string }> = [];
-		for (const line of this.lines) {
-			if (line.key != null && typeof line.key === 'object') line.key = await this.resolveLine(line.key);
-			if (line.value != null && typeof line.value === 'object') line.value = await this.resolveLine(line.value);
-
-			lines.push(line as { key?: string, value: string });
-		}
 		const paddingSize = this.lines.reduce((a, line) =>
 			line.key != null && line.key.toString().length > a ? line.key.toString().length : a, 0,
 		);
 
 		// add lines
-		for (const line of lines) {
+		for (const line of this.lines) {
 			if (line.key) render += `${line.key.toString().padStart(paddingSize)} : `;
 			render += `${line.value.toString()}\n`;
 		}
