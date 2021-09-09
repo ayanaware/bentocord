88c822ec74c6d67b1f7f8371311ea3ca76809dd8
HcgRandon <me@randon.moe>
2021-09-08 21:55:44 -0400

    m0ar types

--- lib/builders/LocalizedCodeblockBuilder.ts
+++ lib/builders/LocalizedCodeblockBuilder.ts
@@ -1,6 +1,6 @@
 import { CommandContext } from '../commands/CommandContext';
 
-import { CodeblockBuilder } from './CodeblockBuilder';
+import { CodeblockLineItem, CodeblockBuilder } from './CodeblockBuilder';
 
 export type LocalizedLineItem = string | { key: string, repl?: Record<string, unknown> };
 
@@ -13,11 +13,11 @@ export class LocalizedCodeblockBuilder extends CodeblockBuilder {
 		this.ctx = ctx;
 	}
 
-	public async addTranslatedLine(key: LocalizedLineItem, value: LocalizedLineItem): Promise<LocalizedCodeblockBuilder> {
+	public async addTranslatedLine(key: LocalizedLineItem, value: LocalizedLineItem | CodeblockLineItem): Promise<LocalizedCodeblockBuilder> {
 		if (typeof key === 'string') key = { key };
 		const keyTranslated = await this.ctx.getTranslation(key.key, key.repl);
 
-		let valueTranslated: string;
+		let valueTranslated: CodeblockLineItem;
 		if (typeof value === 'object') valueTranslated = await this.ctx.getTranslation(value.key, value.repl);
 		else valueTranslated = value;
 
