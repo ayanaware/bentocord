285e538c3fbacb7af5a22278b0ca1d247f3c7db2
HcgRandon <me@randon.moe>
2021-09-28 12:44:44 -0400

    fix insta close

--- lib/prompt/prompts/ConfirmPrompt.ts
+++ lib/prompt/prompts/ConfirmPrompt.ts
@@ -1,6 +1,6 @@
-
 import { Message } from 'eris';
 
+import { Translateable } from '../../interfaces/Translateable';
 import { PROMPT_CLOSE } from '../Prompt';
 
 import { PaginationPrompt } from './PaginationPrompt';
@@ -11,6 +11,20 @@ export class ConfirmPrompt extends PaginationPrompt<boolean> {
 
 		const reason = await this.ctx.formatTranslation('BENTOCORD_PROMPT_CANCELED_TIMEOUT') || 'You took too much time to respond.';
 		return this.close(reason);
+	}
+
+	public async open(content: string | Translateable): Promise<boolean> {
+		if (typeof content === 'object') content = await this.ctx.formatTranslation(content.key, content.repl) || content.backup;
+		this.content = content;
+
+		await this.render();
+
+		// not a single page add reactions & start prompt
+		if (!this.isSinglePage) {
+			this.addReactions().catch(() => { /* no-op */ });
+		}
+
+		return this.start();
 	}
 
 	public async handleResponse(input: string, message?: Message): Promise<void> {
